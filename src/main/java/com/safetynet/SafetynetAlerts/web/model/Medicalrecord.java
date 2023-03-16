92683094941717c1e46f2d56eb131ccb98519406
Said Hadj-Larbi <said.hadjlarbi@bbox.fr>
2023-03-16 09:25:24 +0100

    Création des model : Person, Firestation, Medicalrecord

---
+++ src/main/java/com/safetynet/SafetynetAlerts/web/model/Medicalrecord.java
@@ -0,0 +1,75 @@
+package com.safetynet.SafetynetAlerts.web.model;
+
+
+import java.util.ArrayList;
+
+public class Medicalrecord{
+    private String firstName;
+    private String lastName;
+    private String birthdate;
+    private ArrayList<String> medications;
+    private ArrayList<String> allergies;
+
+    public Medicalrecord() {
+    }
+
+    public Medicalrecord(String firstName, String lastName, String birthdate, ArrayList<String> medications, ArrayList<String> allergies) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthdate = birthdate;
+        this.medications = medications;
+        this.allergies = allergies;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public ArrayList<String> getMedications() {
+        return medications;
+    }
+
+    public void setMedications(ArrayList<String> medications) {
+        this.medications = medications;
+    }
+
+    public ArrayList<String> getAllergies() {
+        return allergies;
+    }
+
+    public void setAllergies(ArrayList<String> allergies) {
+        this.allergies = allergies;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Medicalrecord{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthdate='" + birthdate + '\'' +
+                ", medications=" + medications +
+                ", allergies=" + allergies +
+                '}';
+    }
+}
